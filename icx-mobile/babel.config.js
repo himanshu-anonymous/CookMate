7d1809656aff7c2646493f75539a2d58adcc990e
Sapphire <79043349+himanshu-anonymous@users.noreply.github.com>
2026-01-11 16:53:48 +0530

    Remove react-native-reanimated plugin from Babel config
    Removed problematic react-native-reanimated plugin from Babel config.

--- icx-mobile/babel.config.js
+++ icx-mobile/babel.config.js
@@ -4,7 +4,7 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       "nativewind/babel",
-      // "react-native-reanimated/plugin"  <-- THIS WAS THE PROBLEM. IT IS GONE NOW.
+     
     ],
   };
 };
